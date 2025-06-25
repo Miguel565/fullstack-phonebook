9558b78b2118f080c2cfe78aa3fa54507f3d5d97
Miguel Guerrero <miguel.guerrerog6@gmail.com>
2025-06-24 22:21:29 -0500

    3.4: Phonebook, step 4

--- index.js
+++ index.js
@@ -45,6 +45,13 @@ app.get('/api/persons/:id', (req, res) => {
     }
 })
 
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    contacts = contacts.filter(contact => contact.id !== id)
+
+    res.status(204).end()
+})
+
 app.get('/info', (req, res) => {
     const date = new Date()
     const count = contacts.length
