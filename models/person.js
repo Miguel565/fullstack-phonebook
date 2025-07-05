6fcbb65eeb7db50d714f3348606c0680437e6521
Miguel Guerrero <miguel.guerrerog6@gmail.com>
2025-07-04 19:46:20 -0500

    3.20: Phonebook's database, step 8

--- models/person.js
+++ models/person.js
@@ -18,7 +18,18 @@ const personSchema = new mongoose.Schema({
         maxLength: 20,
         required: true
     },
-    number: Number,
+    number: {
+        type: String,
+        minLength: 8,
+        maxLength: 10,
+        validate: {
+            validator: function(v){
+                return /\d{2,3}-\d{4,7}/.test(v);
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        },
+        required: [true, 'Number is required']
+    }
 })
 personSchema.set('toJSON', {
     transform: (document, returnedObject) => {
