f51d97cd37ac57f6214298dfa7e8ff7ffb422791
Miguel Guerrero <miguel.guerrerog6@gmail.com>
2025-08-31 11:11:56 -0500

    'deploy'

--- models/person.js
+++ models/person.js
@@ -19,12 +19,12 @@ const personSchema = new mongoose.Schema({
         required: true
     },
     number: {
-        type: String,
-        minLength: 8,
+        type: Number,
+        minLength: 6,
         maxLength: 10,
         validate: {
             validator: function(v){
-                return /\d{2,3}-\d{4,8}/.test(v);
+                return /\d{6,10}/.test(v);
             },
             message: props => `${props.value} is not a valid phone number!`
         },
